d0ffbaa067ba22816c7aa6bb75fd00396cc31663
mesh_fukuda <fukuda@skeiei-11.alps.pref.nagano.lg.jp>
2024-01-22 15:37:16 +0900

    first commit

---
+++ precache-manifest.da021bfdf6a8684f20271b8fba0f2ce1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c74e0a3b5a9819facbf",
+    "url": "/koshi/static/css/main.beef46d8.chunk.css"
+  },
+  {
+    "revision": "1c74e0a3b5a9819facbf",
+    "url": "/koshi/static/js/main.1c74e0a3.chunk.js"
+  },
+  {
+    "revision": "6da2dfa5d45f7e9e9fc6",
+    "url": "/koshi/static/js/1.6da2dfa5.chunk.js"
+  },
+  {
+    "revision": "d90c92117e3886484d32",
+    "url": "/koshi/static/js/runtime~main.d90c9211.js"
+  },
+  {
+    "revision": "4421deabfbd7cdbffe98873156f6650d",
+    "url": "/koshi/index.html"
+  }
+];
